3dc329dc306e8321269ecf02d255e05379b5a1fa
tohewi <82122655+tohewi@users.noreply.github.com>
2026-05-24 12:30:16 +0300

    R100 add registration message catalog

---
+++ scoring-proxy/lib/services/registration-messages.js
@@ -0,0 +1,17 @@
+export const REGISTRATION_MESSAGES_FI = {
+  validationInvalid: 'Virheelliset tiedot.',
+  captchaExpired: 'Captcha vanhentunut. Päivitä sivu ja yritä uudelleen.',
+  captchaWrong: 'Väärä vastaus.',
+  cupFull: 'Tapahtuma on täynnä.',
+  squadFull: 'Valittu squad on täynnä.',
+  capacityFull: 'Tapahtuma tai squad on täynnä.',
+  registrationReceived: 'Ilmoittautuminen vastaanotettu.',
+  registrationSynced: 'Ilmoittautuminen onnistui ja SSI-squadiin asettelu onnistui.',
+  registrationManualNeeded: 'Ilmoittautuminen vastaanotettu. Järjestäjä näkee ilmoittautumisesi osallistujalistalla.',
+  registrationPartial: 'Ilmoittautuminen vastaanotettu. SSI-käsittely onnistui osittain ja järjestäjä tarkistaa tilanteen.',
+  registrationSyncFailed: 'Ilmoittautuminen vastaanotettu. SSI-käsittely epäonnistui, mutta järjestäjä näkee ilmoittautumisesi.',
+}
+
+export function registrationMessage(key) {
+  return REGISTRATION_MESSAGES_FI[key] || REGISTRATION_MESSAGES_FI.registrationReceived
+}
